3f70067f2ab19ad163c9cd067f3aeb8ee5c1d6f3
mnagt <112927152+mnagt@users.noreply.github.com>
2026-01-20 11:04:11 +0300

    edt

--- l10n_tr_nilvera_einvoice_usd/models/account_move.py
+++ l10n_tr_nilvera_einvoice_usd/models/account_move.py
@@ -9,8 +9,7 @@ _logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _name = 'account.move'
-    _inherit = ['account.move']
-
+    _inherit = 'account.move'
 
 
     def _l10n_tr_nilvera_submit_document(self, xml_file, endpoint, post_series=True):
@@ -71,4 +70,7 @@ class AccountMove(models.Model):
                 raise UserError(_("Server error from Nilvera, please try again later."))
     
             self.message_post(body=_("The invoice has been successfully sent to Nilvera."))
+
+    def button_draft(self):
+        super().button_draft
 
