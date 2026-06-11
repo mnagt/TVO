9007bdfb0404c668cf47e503b8fcb2a91a207d1f
mnagt <112927152+mnagt@users.noreply.github.com>
2026-06-11 17:08:36 +0300

    Add module for team security.

---
+++ sale_extension_team_security/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Sale Team Security',
+    'version': '18.0.1.0.0',
+    'category': 'Sales/Sales',
+    'depends': ['sales_team', 'sale'],
+    'data': ['security/sales_team_security.xml'],
+    'auto_install': False,
+}
